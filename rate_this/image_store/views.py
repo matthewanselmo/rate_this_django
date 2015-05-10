8362d528fbc8314e2543dd284dd273685fc9a241
Matthew Anselmo <matthewanselmo@outlook.com>
2015-05-10 18:38:19 -0400

    Changed rendering of templates
    - now using render() instead of render_to_redirect()

--- rate_this/image_store/views.py
+++ rate_this/image_store/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render
+
 from .forms import UserImageForm
 
 
 def index(request):
-    return render_to_response('image_store/index.html')
+    return render(request, 'image_store/index.html')
 
 
 def uploadImage(request):
-    context = RequestContext(request)
 
     if request.method == 'POST':
         user_image_form = UserImageForm(request.POST, request.FILES)
@@ -24,5 +23,5 @@ def uploadImage(request):
     else:
         user_image_form = UserImageForm()
 
-    return render_to_response('image_store/upload_image.html', {
-        'user_image_form': user_image_form}, context)
+    return render(request, 'image_store/upload_image.html', {
+        'user_image_form': user_image_form})
